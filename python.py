58e97349d581b4c3cc127f3dd4d6a6b1b77aff89
Mohit Sharma <shash199655@gmail.com>
2020-10-01 21:36:25 +0530

    added my name :)
    please merge this pull request

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 print("Laço de repetição para imprimir todos os elementos da lista")
 
-nomes = ['Pedro', 'João', 'Leticia', 'Matheus', 'Paulo', 'Lucas']
+nomes = ['Pedro', 'João', 'Leticia', 'Matheus', 'Paulo', 'Lucas', 'Granny']
 
 for n in nomes:
     print(n)
